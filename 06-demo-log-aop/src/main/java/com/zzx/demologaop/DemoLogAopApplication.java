4471035fbe45d9c3128489c52ce147d501e920a3
yun.yin <zzx_5210@163.com>
2021-02-22 09:56:51 +0800

    add 06-demo-log-aop module

---
+++ 06-demo-log-aop/src/main/java/com/zzx/demologaop/DemoLogAopApplication.java
@@ -0,0 +1,18 @@
+package com.zzx.demologaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动类
+ * </p>
+ */
+@SpringBootApplication
+public class DemoLogAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLogAopApplication.class, args);
+    }
+
+}
---
+++ 06-demo-log-aop/src/test/java/com/zzx/demologaop/DemoLogAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.demologaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
